917f2cf849abde435d02528820538a933d310748
Lucas Racoci <racoci.0@gmail.com>
2025-01-22 18:54:53 -0300

    Create components and add markups.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {HeaderComponent} from "./header/header.component";
+import {UserInputComponent} from "./user-input/user-input.component";
+import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [
+    NgOptimizedImage,
+    HeaderComponent,
+    UserInputComponent,
+    InvestmentResultsComponent
+  ]
 })
 export class AppComponent {}
