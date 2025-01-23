a6459a94e26ba038b2aa1155dd7e349dea185cd0
Lucas Racoci <racoci.0@gmail.com>
2025-01-23 13:10:57 -0300

    Create and use AppModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,7 @@
 import {Component} from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
-import {UserInputComponent} from "./user-input/user-input.component";
-import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
-import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  imports: [
-    HeaderComponent,
-    UserInputComponent,
-    InvestmentResultsComponent,
-    FormsModule
-  ]
 })
 export class AppComponent {}
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,16 +1,9 @@
 import {Component} from '@angular/core';
 import {InvestmentResult} from "./investment-results.model";
-import {CurrencyPipe, NgForOf, NgIf} from "@angular/common";
 import {InvestmentService} from "../investment.service";
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [
-    NgForOf,
-    CurrencyPipe,
-    NgIf
-  ],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
