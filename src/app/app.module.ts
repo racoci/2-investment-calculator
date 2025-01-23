89fdc1ea22769f30c9135b7816339d04567c247b
Lucas Racoci <racoci.0@gmail.com>
2025-01-23 13:14:33 -0300

    Create and use user input module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
-import {UserInputComponent} from "./user-input/user-input.component";
 import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
-import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
+import {UserInputModule} from "./user-input/user-input.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserInputComponent,
     InvestmentResultsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    UserInputModule
   ],
   bootstrap: [AppComponent]
 })
